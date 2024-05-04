8f80b778a5bb66b77bdfe39bb858bcf0986b4d7f
Kirill <104451159+Modestra@users.noreply.github.com>
2024-05-04 10:40:03 +0700

    Настройка среды

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,27 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  root: './src/app',
+  server: {
+    port: 8080,
+    proxy: {
+      '/api': {
+        //changeOrigin: true,
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@app': fileURLToPath(new URL('./src/app', import.meta.url)),
+      '@styles': fileURLToPath(new URL('./src/app/styles', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/app/assets', import.meta.url))
     }
   }
 })
